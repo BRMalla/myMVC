4484e25b9d0e18826b72e0ff2ed5f3d717292942
BR  Malla <brmalla3@gmail.com>
2018-03-19 05:57:47 +0545

    supermarkent management system created

---
+++ supermarket/supermarket/BLL/business_layer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+	class business_layer
+	{
+		rank rk = new rank();
+		public ActionResult login_admin()
+		{
+			return View();
+		}
+	}
+}
